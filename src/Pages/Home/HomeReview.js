c3b4d8f4fe38237eee5e136b8881f36b73912779
SIArifBD <si.arif.cse@gmail.com>
2022-05-27 22:03:26 +0600

    9th commit

--- src/Pages/Home/HomeReview.js
+++ src/Pages/Home/HomeReview.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HomeReview = ({ review }) => {
-    const { rating, image, description, displayName, email } = review;
+    const { rating, photoURL, description, displayName, email } = review;
     return (
         <div className="card lg:max-w-lg bg-base-100 shadow-xl">
             <div className="card-body">
@@ -10,7 +10,7 @@ const HomeReview = ({ review }) => {
             <div className='flex items-center my-3'>
                 <div className="avatar">
                     <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
-                        <img src={image} alt="" />
+                        <img src={photoURL} alt="" />
                     </div>
                 </div>
                 <div>
